27fb96441d337f235e97ea37f26b027b972a67a3
Andrew Williams <1williamsaj@gmail.com>
2019-10-10 02:39:06 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select {|apts| apts.patient == self}
+   Appointment.all.select {|appointment| appointment.patient == self}
   end
 
   def new_appointment(date, doctor)
