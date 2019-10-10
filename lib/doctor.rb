bf90861c2c69e6cdcb6629b7edf077997ef7ef5e
Andrew Williams <1williamsaj@gmail.com>
2019-10-10 03:04:57 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,8 +17,8 @@ class Doctor
     Appointment.all
   end
 
-  def new_appointment(patient, date)
-    Appointment.new(patient, self, date)
+  def new_appointment(date, patient)
+    Appointment.new(patient, self)
   end
 
   def patients
